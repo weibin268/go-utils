1e74f375c525cff96f638f7901cb8ffd76f7dcdd
zwb <binweizhuang@hotmail.com>
2022-10-10 22:29:56 +0800

    aa

--- io/file_test.go
+++ io/file_test.go
@@ -3,6 +3,6 @@ package io
 import "testing"
 
 func TestReadAllText(t *testing.T) {
-	text := ReadAllText("/Users/zhuang/Documents/temp/test.txta")
+	text := ReadAllText("/Users/zhuang/Documents/temp/test.txt")
 	t.Log(text)
 }
